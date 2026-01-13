310e1dfe25722cae6f549c5851517f9bbbf72abd
robertjahn <robjahn@gmail.com>
2026-01-13 17:54:48 -0500

    add devcontainer files

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,21 @@
+HOST_BASE_URL=http://52.186.168.229:3800
+
+export DT_BASE_URL=$(curl -s -X POST $HOST_BASE_URL/dynatrace-url \
+  -H "Content-Type: application/json" \
+  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.dynatrace_url')
+
+export DYNATRACE_API_TOKEN=$(curl -s -X POST $HOST_BASE_URL/dynatrace-token \
+  -H "Content-Type: application/json" \
+  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.dynatrace_api_token')
+
+export NVIDIA_API_KEY=$(curl -s -X POST $HOST_BASE_URL/nvidia-key \
+  -H "Content-Type: application/json" \
+  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.nvidia_api_key')
+
+export TAVILY_API_KEY=$(curl -s -X POST $HOST_BASE_URL/tavily-key \
+  -H "Content-Type: application/json" \
+  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.tavily_api_key')
+
+export OTEL_OTLP_ENDPOINT=$(curl -s -X POST $HOST_BASE_URL/otel-endpoint \
+  -H "Content-Type: application/json" \
+  -d "{\"password\": \"$WORKSHOP_PASSWORD\"}" | jq -r '.otel_otlp_endpoint')
